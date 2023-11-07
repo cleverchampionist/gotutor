16cb5de78040e969688fbe8c163b4714afd3454d
vasyl <vasyl.savuliak@outlook.com>
2023-11-07 01:32:20 -0800

    Go lang web initial sever!

--- helloworld.go
+++ helloworld.go
@@ -2,8 +2,17 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("Hello World!")
+	http.HandleFunc("/", MyHandler1)
+	http.HandleFunc("/john", MyHandler2)
+	http.ListenAndServe(":8081", nil)
+}
+func MyHandler1(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello World\n")
+}
+func MyHandler2(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello John\n")
 }
